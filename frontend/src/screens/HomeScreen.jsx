c4bdd8fe79f643aa0ca6e350dfd8fa2123a7036e
Somvaded <sovajitr@gmail.com>
2023-06-24 21:30:28 +0530

    Started backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {};
+  });
   return (
     <>
       <h1>Latest Products</h1>
